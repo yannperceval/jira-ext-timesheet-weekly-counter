c843bc158e7bb693e0c0919505a373e8adfac89f
Yann Roseau <9842982+yroseau@users.noreply.github.com>
2019-05-14 18:31:55 +0200

    Disable the extension

--- timesheet-weekly-counter.js
+++ timesheet-weekly-counter.js
@@ -12,48 +12,50 @@
 // @updateURL    https://raw.githubusercontent.com/yroseau/jira-extension/master/timesheet-weekly-counter.js
 // ==/UserScript==
 
-var $tempoTable = $(".tempo-timesheet-table");
+// disable temporary the extension
 
-if ($tempoTable.length) {
+// var $tempoTable = $(".tempo-timesheet-table");
 
-  $('body').append('<style>\
-        .day { position: relative; } \
-        .weekHours { \
-          background-color: #3b73af; \
-          color: white; \
-          font-size: 0.75em; \
-          position:absolute; \
-          right: 0; \
-          top: 0; \
-          line-height: 0.5em; \
-          padding: 0.5em 0.5em; \
-          border-radius: 0 0 0 0.75em; \
-          text-shadow: 0 0 4px #0d0d0d6b; \
-        } \
-        .day:hover .weekHours { \
-          opacity: 0; \
-          display: none; \
-        } \
-    </style>');
+// if ($tempoTable.length) {
 
-  var $headerRows = $tempoTable.find('.rowNormal');
-  $headerRows.each(function () {
-    var $days = $(this).find('.day');
-    var count = 0;
-    var length = $days.length;
-    $days.each(function (index) {
-      var hStr = $(this).text().trim();
-      if (hStr !== "") {
-        var h = parseFloat(hStr);
-        if (!isNaN(h)) {
-          count += h;
-        }
-      }
-      if (count !== 0 && ($(this).is('.tt-end-of-week') || index === length - 1)) {
-        // count = Math.round(count);
-        $(this).append('<div class="weekHours">' + count + '</div>');
-        count = 0;
-      }
-    });
-  });
-}
+//   $('body').append('<style>\
+//         .day { position: relative; } \
+//         .weekHours { \
+//           background-color: #3b73af; \
+//           color: white; \
+//           font-size: 0.75em; \
+//           position:absolute; \
+//           right: 0; \
+//           top: 0; \
+//           line-height: 0.5em; \
+//           padding: 0.5em 0.5em; \
+//           border-radius: 0 0 0 0.75em; \
+//           text-shadow: 0 0 4px #0d0d0d6b; \
+//         } \
+//         .day:hover .weekHours { \
+//           opacity: 0; \
+//           display: none; \
+//         } \
+//     </style>');
+
+//   var $headerRows = $tempoTable.find('.rowNormal');
+//   $headerRows.each(function () {
+//     var $days = $(this).find('.day');
+//     var count = 0;
+//     var length = $days.length;
+//     $days.each(function (index) {
+//       var hStr = $(this).text().trim();
+//       if (hStr !== "") {
+//         var h = parseFloat(hStr);
+//         if (!isNaN(h)) {
+//           count += h;
+//         }
+//       }
+//       if (count !== 0 && ($(this).is('.tt-end-of-week') || index === length - 1)) {
+//         // count = Math.round(count);
+//         $(this).append('<div class="weekHours">' + count + '</div>');
+//         count = 0;
+//       }
+//     });
+//   });
+// }
